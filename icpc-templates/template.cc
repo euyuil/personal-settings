eb9c4429e5821d3b10a8100f70668192a04e8077
Liu Yue <euyuil@gmail.com>
2013-09-05 09:44:06 +0800

    Added ICPC templates.

---
+++ icpc-templates/template.cc
@@ -0,0 +1,23 @@
+#include <map>
+#include <set>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <cstdio>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <climits>
+#include <utility>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main()
+{
+
+    return EXIT_SUCCESS;
+}
